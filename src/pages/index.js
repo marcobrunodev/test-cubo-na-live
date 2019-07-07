021cfb8e6e02e6d98c3d5eb6b7652304fe75b9d0
Marco Bruno <marco.bruno.br@gmail.com>
2019-07-07 02:46:50 +0100

    Finished HeaderCubo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react"
 
-import "../styles/settings/colors.css"
-import "../styles/settings/size.css"
+import "../styles/settings/index.css"
+import "../styles/tools/tricks.css"
+import "../styles/generic/reset.css"
+import "../styles/elements/base.css"
 
 import HeaderCubo from "../components/HeaderCubo"
 
